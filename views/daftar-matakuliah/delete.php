39c13c8e6f46dcb43154f6cb0d7c58b01efa54df
woda07 <farhan.woda@gmail.com>
2023-05-09 16:15:21 +0800

    first commit

---
+++ views/daftar-matakuliah/delete.php
@@ -0,0 +1,6 @@
+<?php
+
+/** @var yii\web\View $this */
+?>
+
+<h1>daftar-matakuliah/delete</h1>
---
+++ views/mahasiswa/profil.php
@@ -0,0 +1,6 @@
+<B>
+Nama    : Muh. Farhan <br>
+NIM     : 60200121044 <br>
+Kelas   : B <br>
+Jurusan : Teknik Informatika <br>
+</B>
